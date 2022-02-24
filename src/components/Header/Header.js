83328948e7eaa2a8dfd9e4dac4f46b7b0832b2e0
Israel-Beni <bendegbe72@gmail.com>
2022-02-24 21:36:14 +0530

    menuItems click functionality added and Header.js completedf

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,39 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            page: 'home'
+            page: 'portfolio'
         }
+        this.menuItems = {
+            home: 'HOME',
+            portfolio: 'PORTFOLIO',
+            contact: 'CONTACT ME'
+        }
+        this.activateMenuItem = this.activateMenuItem.bind(this);
+        this.handlePageChange = this.handlePageChange.bind(this); // sets this.state.page
+        this.renderMenu = this.renderMenu.bind(this);
     }
+
+    activateMenuItem(menuItem) {
+        return this.state.page === menuItem ? 'activate' : '';
+    }
+
+    handlePageChange(menuItem) {
+        this.setState({
+            page: menuItem
+        });
+    }
+
+    renderMenu() {
+        return Object.keys(this.menuItems).map( menuItem => {
+            const menuItemValue = this.menuItems[menuItem];
+            return <li  key={menuItemValue}
+                        className={this.activateMenuItem(menuItemValue)}
+                        onClick={ event => this.handlePageChange(menuItemValue)}
+                    >{menuItemValue}</li>
+        })
+    }
+
+
     render() {
         return (
             <div className="header">
@@ -16,9 +46,7 @@ class Header extends React.Component {
                     <img src={logo} alt="logo" />
                 </div>
                 <ul className="navigation_menu">
-                    <li className="home">Home</li>
-                    <li className="portfolio">Portfolio</li>
-                    <li className="contact_me">Contact Me</li>
+                    {this.renderMenu()}
                 </ul>
             </div>
         );
