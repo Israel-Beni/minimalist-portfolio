f512b6c208b683f6e2de7c313a7754263abc48af
Israel-Beni <bendegbe72@gmail.com>
2022-02-26 23:11:03 +0530

    FooterBar component added to Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Footer.css';
 import ContactLink from '../ContactLink/ContactLink';
+import FooterBar from '../FooterBar/FooterBar';
 
 class Footer extends React.Component {
     render() {
         return (
             <div class="Footer">
                 <ContactLink />
-                
+                <FooterBar />
             </div>
         );
     }
---
+++ src/components/FooterBar/FooterBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './FooterBar.css';
+
+class FooterBar extends React.Component {
+    render() {
+        return (
+            <div className="FooterBar">
+             
+            </div>
+        );
+    }
+};
+
+export default FooterBar;
