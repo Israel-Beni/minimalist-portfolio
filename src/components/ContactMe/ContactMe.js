e7fc4130e1f2901cb8fc6e7f8d5bb2c5a3303100
Israel-Beni <bendegbe72@gmail.com>
2022-03-02 16:31:30 +0530

    ContactMe component Folder added to working directory

---
+++ src/components/ContactMe/ContactMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ContactMe.css';
+
+class ContactMe extends React.Component {
+    render() {
+        return (
+            <section className="ContactMe"></section>
+        );
+    }
+};
+
+export default ContactMe;
