3b7ee243f38a4eb9766419dac7951739a69b7c00
Israel-Beni <bendegbe72@gmail.com>
2022-03-02 22:48:30 +0530

    ContactForm component JSX written

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,9 +4,19 @@ import './ContactForm.css';
 class ContactForm extends React.Component {
     render() {
         return (
-            <form className="ContactForm">
-             
-            </form>
+            <section className="ContactForm">
+                <h2>Contact Me</h2>
+                <form action="">
+                    <label for="name">Name</label><br />
+                    <input id="name" name="name" type="text" placeholder="Jane Appleseed" /><br />
+                    <label for="email">Email Address</label><br />
+                    <input id="email" name="email" type="email" placeholder="email@example.com" /><br />
+                    <label for="message">Message</label><br />
+                    <textarea id="message" name="message">
+                        How can I help?
+                    </textarea>
+                </form>
+            </section>
         );
     }
 };
