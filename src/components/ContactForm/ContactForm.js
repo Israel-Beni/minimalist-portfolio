7174ede994c19f719376af37e062e66fec9a56f4
Israel-Beni <bendegbe72@gmail.com>
2022-03-02 18:51:25 +0530

    ContactForm added to the project

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContactForm.css';
+
+class ContactForm extends React.Component {
+    render() {
+        return (
+            <form className="ContactForm">
+             
+            </form>
+        );
+    }
+};
+
+export default ContactForm;
--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './ContactMe.css';
 import GetInTouch from '../GetInTouch/GetInTouch';
+import ContactForm from '../ContactForm/ContactForm';
 
 class ContactMe extends React.Component {
     render() {
         return (
             <section className="ContactMe">
                 <GetInTouch />
+                <ContactForm />
             </section>
         );
     }
