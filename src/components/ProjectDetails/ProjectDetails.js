58a55d31a2a5480bd716965228b2d9876939f2c2
Israel-Beni <bendegbe72@gmail.com>
2022-04-02 21:39:16 +0530

    ProjectDetails component added

---
+++ src/components/ProjectDetails/ProjectDetails.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ProjectDetails.css';
+
+class ProjectDetails extends React.Component {
+    render() {
+        return ();
+    }
+};
+
+export default ProjectDetails;
