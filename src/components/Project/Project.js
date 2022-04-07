4627eea7d91837164eb32ad63afe42891ae57ba4
Israel-Beni <bendegbe72@gmail.com>
2022-04-07 10:42:04 +0530

    desktop styling for Project component written

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -11,20 +11,21 @@ class Project extends React.Component {
 
     swatchModels() {
         return this.props.model === 'thumbnail-first' ? (
-            <div className="Project">
+            <div className="thumbnail-first Project-container">
                 <div className="thumbnail-container">
                     <img src="" alt="" />
                 </div>
                 <div className="info">
                     <h2>Manage</h2>
-                    <p>
+                    <p class="description">
                         This project required me to build a fully responsive landing
                         page to the designs provided. I used HTLM5 along with CSS Grid
                         and JavaScript for the areas that required interactivity, such as the testimonial
                         slider.
                     </p>
                     <button>
-                        <Link to="/portfolio/manage">
+                        <Link to="/portfolio/manage"
+                              style={{ textDecoration: "none", color: "#33323D"}}>
                             {"View Project".toUpperCase()}
                         </Link>
                     </button>
