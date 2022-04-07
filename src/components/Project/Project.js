46c0ccb7ebce0102999ae7a7a41dae94b6d88dcb
Israel-Beni <bendegbe72@gmail.com>
2022-04-07 21:47:38 +0530

    loading icon added: portfolio page will show loading when data is now yet fetched

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,24 +2,30 @@ import React from 'react';
 import './Project.css';
 import { getData } from '../../api/api';
 
+import portfolioImage from '../../images/portfolio/desktop/image-portfolio-manage.jpg'
+import loading from '../../images/icons/icons8-spinner.gif'
+
 import { Link } from 'react-router-dom';
 
 class Project extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: {}
+        };
         this.swatchModels = this.swatchModels.bind(this);
     }
 
-    swatchModels() {
-        // const jsonObject = JSON.parse(getData());
-        console.log("getData: ", getData());
-        return this.props.model !== 'thumbnail-first' ? (
+     swatchModels() {
+         const data = this.state.data;
+        if (!data) {
+        return this.props.model === 'thumbnail-first' ? (
             <div className="thumbnail-first Project-container">
                 <div className="thumbnail-container">
-                    <img src="" alt="" />
+                    <img src={portfolioImage} alt="" />
                 </div>
                 <div className="info">
-                    <h2>Manage</h2>
+                    <h2>{}</h2>
                     <p className="description">
                         This project required me to build a fully responsive landing
                         page to the designs provided. I used HTLM5 along with CSS Grid
@@ -56,20 +62,37 @@ class Project extends React.Component {
                 </div>
             </div>
         );
+        } else {
+            setTimeout( () => {
+                    const portfolio = document.querySelector('.Portfolio');
+                    console.log('portfolio: ', portfolio)
+                    portfolio.innerHTML = `<div className="loading"><img src=${loading} alt="" /></div>`;
+                    portfolio.style.width = '100px';portfolio.style.width = '100px';
+                    portfolio.style.margin = 'auto';
+                    
+                    const image = document.querySelector('.Portfolio img');
+                    image.style.width = '50px';
+                    image.style.height = '50px';
+                    
+
+                    });
+
+    }}
+
+    async componentDidMount() { this.setState({
+        data: await getData()
+    });
+        console.log('getData\'s name from componentDidMount: ',  this.state.data);
     }
 
-    async componentDidMount() {
-        const projects = await getData();
-        console.log('getData from componentDidMount: ', projects);
-    }
-
-    componentDidUpdate() {
-        console.log('getData from componentDidUpdate: ', getData())
+    async componentDidUpdate() {
+         console.log('getData from componentDidUpdate: ',  this.state.data)
     }
 
     render() {
         return (
             <div className="Project">
+                {console.log(this.state.data[0])}
                 {this.swatchModels()}
             </div>
         );
