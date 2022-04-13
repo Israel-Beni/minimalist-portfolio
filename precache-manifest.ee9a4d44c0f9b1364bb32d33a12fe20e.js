c7e300fb6b157bbb1d15149a026b691a380bd376
Israel-Beni <bendegbe72@gmail.com>
2022-04-13 21:58:50 +0530

    Updates

---
+++ precache-manifest.ee9a4d44c0f9b1364bb32d33a12fe20e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df56583ee216565c6f0dd32e16ef603d",
+    "url": "/minimalist-portfolio/index.html"
+  },
+  {
+    "revision": "61eb57f2c307ddf7b9c2",
+    "url": "/minimalist-portfolio/static/css/main.6ea5275f.chunk.css"
+  },
+  {
+    "revision": "3a01fc3b4fd80d3835aa",
+    "url": "/minimalist-portfolio/static/js/2.c8034ccc.chunk.js"
+  },
+  {
+    "revision": "a9807912173ef280ea3f",
+    "url": "/minimalist-portfolio/static/js/3.9f01db21.chunk.js"
+  },
+  {
+    "revision": "61eb57f2c307ddf7b9c2",
+    "url": "/minimalist-portfolio/static/js/main.0816c813.chunk.js"
+  },
+  {
+    "revision": "7a31eb7437f6cd9886a4",
+    "url": "/minimalist-portfolio/static/js/runtime~main.60627a34.js"
+  },
+  {
+    "revision": "2e4f457fc47854d6cae5186e0d85111e",
+    "url": "/minimalist-portfolio/static/media/image-homepage-hero@2x.2e4f457f.jpg"
+  }
+]);
